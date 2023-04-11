3f6805fdfbb79023fa1e07c2d3c9d988b856d782
realizer <bonfireof@gmail.com>
2023-04-11 14:40:30 +0900

    tiny utils for me
    basic utils and logger are created.

--- test/KRLog.py
+++ test/KRLog.py
@@ -7,8 +7,9 @@ LOG처리
 
 
 
+
 # 로그 설정값을 관리
-class _config:
+class _logger_config:
 
     def __init__(self):
         ######################
@@ -21,38 +22,29 @@ class _config:
         self._LOG_SUBSTITUTOR    = "%%";
 
         # 시스템 로그를 포함하여 출력
-        self.DEBUG_LEVEL_ALL    = _util().get_log_level_index("ALL");
+        self.DEBUG_LEVEL_ALL    = _logger_util().get_log_level_index("SYSTEM");
         # # DB접근 로그를 포함하여 출력
-        self.DEBUG_LEVEL_DB     = _util().get_log_level_index("DB");
+        self.DEBUG_LEVEL_DB     = _logger_util().get_log_level_index("DB");
         # # 프로그램 로그를 포함하여 출력
-        self.DEBUG_LEVEL_APP    = _util().get_log_level_index("APP");
+        self.DEBUG_LEVEL_DEBUG  = _logger_util().get_log_level_index("DEBUG");
         # # 중요 정보 발생시 출력
-        self.DEBUG_LEVEL_INFO   = _util().get_log_level_index("INFO");
+        self.DEBUG_LEVEL_INFO   = _logger_util().get_log_level_index("INFO");
         # # 오류 발생시 출력
-        self.DEBUG_LEVEL_ERR    = _util().get_log_level_index("ERR");
+        self.DEBUG_LEVEL_ERROR  = _logger_util().get_log_level_index("ERROR");
 
 
         ######################
         # 설정
 
         # 파일명 지정
+        self.CONFIG_FILE_PATH = './'
         self.CONFIG_FILE_NAME = 'logger.json'
-        self.CONFIG_FILE_PATH = ''
-        #    SAMPLE : logger.json
-        #    {
-        #        "LOG_LEVEL" : "DB",
-        #        "LOG_CONSOLE_YN" : "Y",
-        #        "LOG_FILE_YN" : "N",
-        #        "LOG_DIR_PATH" : "./logs",
-        #        "LOG_FILE_NAME" : "mylog.log"
-        #    }
 
         ######################
         # 로거 동작 설정
         ######################
         # 현재 로그레벨 (최초 생성시 기본로그레벨로 세팅)
-        # self.CURR_DEBUG_LEVEL       = _util().get_log_level_index("ERR");
-        self.CURR_DEBUG_LEVEL       = _util().get_log_level_index("ALL");
+        self.CURR_DEBUG_LEVEL       = _logger_util().get_log_level_index("INFO");
 
         # 디버깅 출력 방법 : 콘솔 출력 여부
         self.DEBUG_CONSOLE_PRINT_YN = "Y";
@@ -69,6 +61,58 @@ class _config:
 
 
 
+class _logger_util:
+
+    def _find_config_file_path(self, start_path: str) -> str:
+
+        import os
+
+        curr_dir = os.path.dirname(start_path)
+        # print("start_path[{0}]".format(curr_dir))
+
+        _cfg = _logger_config()
+
+        config_file_path = ""
+        # while True:
+        for ii in range(5):
+            # print("seeking..[{0}]th : {1}".format(ii, curr_dir))
+
+            if (os.path.isfile(curr_dir + "/" + _cfg.CONFIG_FILE_NAME) == True):
+                config_file_path = os.path.join(curr_dir, _cfg.CONFIG_FILE_NAME)
+                # print("찾았다!", config_file_path)
+                break
+            else:
+                # print("상위로 찾아 올라간다[{0}]->[{1}]".format(curr_dir, os.path.abspath(os.path.join(curr_dir, '..'))))
+                # root까지 확인된 경우 : 더이상 찾을 수 없을 때
+                if (curr_dir == os.path.abspath(os.path.join(curr_dir, '..'))):
+                    # print("못찾았다!")
+                    config_file_path = ''
+                    # raise Exception(_cfg.CONFIG_FILE_NAME + " 파일을 찾을 수 없습니다.")
+                    break
+                else:
+                    curr_dir = os.path.abspath(os.path.join(curr_dir, '..'))
+
+        # print("get_config_file_path() -> [{0}]".format(config_file_path))
+        return config_file_path
+
+
+
+    # 로깅 레벨 설정
+    def get_log_level_index(self, level: str) -> int:
+        if level == None:
+            return 4
+        elif level == "ERROR":
+            return 4
+        elif level == "INFO":
+            return 3
+        elif level == "DEBUG":
+            return 2
+        elif level == "DB":
+            return 1
+        elif level == "SYSTEM":
+            return 0
+        return 4
+
 
 
 # 로깅 클래스의 기능을 담아놓는다.
@@ -79,7 +123,7 @@ class _logging:
         super().__init__()
 
         # 기본 설정 값
-        self.config = _config()
+        self.config = _logger_config()
 
 
 
@@ -119,19 +163,19 @@ class _logging:
 
         header = ""
         if (debug_level == self.config.DEBUG_LEVEL_ALL):
-            header = header + "[SYSTEM]"
+            header = header + "[SYS]"
         elif (debug_level == self.config.DEBUG_LEVEL_DB):
-            header = header + "[  DB  ]"
+            header = header + "[SQL]"
         elif (debug_level == self.config.DEBUG_LEVEL_APP):
-            header = header + "[DEBUG ]"
+            header = header + "[DBG]"
         elif (debug_level == self.config.DEBUG_LEVEL_INFO):
-            header = header + "[ INFO ]"
-        elif (debug_level == self.config.DEBUG_LEVEL_ERR):
-            header = header + "[ERROR ]"
+            header = header + "[INF]"
+        elif (debug_level == self.config.DEBUG_LEVEL_ERROR):
+            header = header + "[ERR]"
 
-        header = header + "[" + datetime.now().strftime("%H%M%S.%f")[:-3] + "]"                             # 일시
-        header = header + "[" + self._caller_file_name() + ":" + str(self._caller_file_line()).zfill(5) + "]"       # 호출자
-        header = header.ljust(HEADER_LENGTH, " ")                                                           # 길이 맞추기
+        header = header + " " + "[" + datetime.now().strftime("%H%M%S.%f")[:-3] + "]"                               # 일시
+        header = header + " " + "[" + self._caller_file_name() + ":" + str(self._caller_file_line()).zfill(5) + "]"       # 호출자
+        header = header.ljust(HEADER_LENGTH, " ")                                                                   # 길이 맞추기
 
 
         return header
@@ -206,12 +250,56 @@ class _logging:
 
 
 
-
-
-
-# 클래스 메인
+######################
+# logger 클래스 메인
 #   1. 설정파일을 찾아 적용한다. 없으면 기본값으로 적용한다.
 class logger(_logging):
+    '''
+    My favorite log format maker.
+    [LEVEL] [TIME] [SOURCE] contents...
+
+    sample>
+        logger = krutils.logger(__file__)
+
+        a = 10.0
+        b = 20.0
+
+        logger.syslog("[%%] %% - {%%}", a, b, a)
+        logger.dblog("[%%] %% - {%%}", a, b, a)
+        logger.debug("[%%] %% - {%%}", a, b, a)
+        logger.info("[%%] %% - {%%}", a, b, a)
+        logger.error("[%%] %% - {%%}", a, b, a)
+
+    result>
+        [SYS] [103350.469] [tester.py:00010] [10.0] 20.0 - {10.0}
+        [SQL] [103350.512] [tester.py:00011] [10.0] 20.0 - {10.0}
+        [DBG] [103350.515] [tester.py:00012] [10.0] 20.0 - {10.0}
+        [INF] [103350.518] [tester.py:00013] [10.0] 20.0 - {10.0}
+        [ERR] [103350.520] [tester.py:00014] [10.0] 20.0 - {10.0}
+
+
+    Options>
+        Set option can redefine with 'logger.json' file.
+        The 'logger.json' file can be caller program directory or above. krutil.logger will seek the 'logger.json' file from caller program directory to root diredctory.
+        First found file will be adapted.
+
+        SAMPLE> 'logger.json'
+        {
+            "LOG_LEVEL" : "INFO",
+            "LOG_CONSOLE_YN" : "Y",
+            "LOG_FILE_YN" : "N",
+            "LOG_DIR_PATH" : "./logs",
+            "LOG_FILE_NAME" : "mylog.log"
+        }
+
+        DESC> * is default value
+        * LOG_LEVEL : SYSTEM/DB/DEBUG/INFO*/ERROR
+        * LOG_CONSOLE_YN : Y*/N
+        * LOG_FILE_YN : Y/N*
+        * LOG_DIR_PATH : log directory path(None is default)
+        * LOG_FILE_NAME : log file name(None is default)
+
+    '''
 
     def __init__(self, caller_path: str):
 
@@ -220,8 +308,9 @@ class logger(_logging):
 
         # 인자로 받은 호출자 경로로 부터 root까지 탐색하며 config 파일을 찾는다.
         # 존재시 설정을 덮어 씌운다
-        self.config.CONFIG_FILE_PATH = _util()._find_config_file_path(caller_path)
+        self.config.CONFIG_FILE_PATH = _logger_util()._find_config_file_path(caller_path)
 
+        # realizer!!! is_empty로 변경, paser 메서드 작성.
         if (len(self.config.CONFIG_FILE_PATH) != 0):
             import json
             with open(self.config.CONFIG_FILE_PATH) as _cf:
@@ -255,76 +344,6 @@ class logger(_logging):
 
 
     def error(self, template="", *args):
-        self._print_log(self.config.DEBUG_LEVEL_ERR, template, *args)
-
-
-
-
-
-
-
-
-
-
-
-class _util:
-
-    def _find_config_file_path(self, start_path: str) -> str:
-
-        import os
-
-        curr_dir = os.path.dirname(start_path)
-#        print("start_path[{0}]".format(curr_dir))
-
-        _cfg = _config()
-
-        config_file_path = ""
-        # while True:
-        for ii in range(5):
-            # print("seeking..[{0}]th : {1}".format(ii, curr_dir))
-
-            if (os.path.isfile(curr_dir + "/" + _cfg.CONFIG_FILE_NAME) == True):
-                config_file_path = os.path.join(curr_dir, _cfg.CONFIG_FILE_NAME)
-                # print("찾았다!", config_file_path)
-                break
-            else:
-                # print("상위로 찾아 올라간다[{0}]->[{1}]".format(curr_dir, os.path.abspath(os.path.join(curr_dir, '..'))))
-                # root까지 확인된 경우 : 더이상 찾을 수 없을 때
-                if (curr_dir == os.path.abspath(os.path.join(curr_dir, '..'))):
-                    # print("못찾았다!")
-                    config_file_path = ''
-                    # raise Exception(_cfg.CONFIG_FILE_NAME + " 파일을 찾을 수 없습니다.")
-                    break
-                else:
-                    curr_dir = os.path.abspath(os.path.join(curr_dir, '..'))
-
-        # print("get_config_file_path() -> [{0}]".format(config_file_path))
-        return config_file_path
-
-
-
-    # 로깅 레벨 설정
-    def get_log_level_index(self, level: str) -> int:
-        if level == None:
-            return 4
-        elif level == "ERR":
-            return 4
-        elif level == "INFO":
-            return 3
-        elif level == "APP":
-            return 2
-        elif level == "DB":
-            return 1
-        elif level == "ALL":
-            return 0
-        return 4
-
-
-
-
-
-
-
-
+        self._print_log(self.config.DEBUG_LEVEL_ERROR, template, *args)
 
 
